5c16c700e0dd2c606a76ceefe178ec35ef2a52a3
t34Virus <treindle@gmail.com>
2023-01-31 16:04:51 -0800

    Update: lock font scaling and restore to default ui settings

--- App.js
+++ App.js
@@ -1,39 +1,46 @@
 import React, {Component} from 'react';
 
 import { StatusBar } from 'expo-status-bar';
-import { Appearance, Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { AppState, Appearance, Platform, StyleSheet, Text, View, Button, useWindowDimensions } from 'react-native';
 import { getLocales } from 'expo-localization';
 
 export default class App extends Component{
-
+ 
   constructor(){
     super();
     this.OS = Platform.OS;
     this.languageCode = getLocales()[0].languageCode;
     this.colorScheme = Appearance.getColorScheme();
+    this.fontSize = 16;
     this.fontScale = 'medium';
-
-    // this.config = {
-    //   en: `Hello, I am an ${this.OS} Expo app on ${this.colorScheme} mode. I speak English but not very well. This device's font is set to.`,
-    //   fr: `Bonjour, je suis une application ${this.OS} Expo en mode ${this.colorScheme === 'dark' ? 'dark' : 'clair'}. Je parle français mais pas bien. La police de cet appareil est définie sur'.`,
-    //   es: `Hola, soy una aplicación de ${this.OS} Expo en modo ${this.colorScheme === 'dark' ? 'oscuro' : 'ligero'}. Hablo español pero no muy bien. La fuente de este dispositivo está configurada en'.`
-    // }
-  
+    this.defaults = [getLocales()[0].languageCode, Appearance.getColorScheme(), 16];
+    // Ignore dynamic type scaling on iOS
+    Text.defaultProps = Text.defaultProps || {};
+    Button.defaultProps = Button.defaultProps || {};
+    Text.defaultProps.allowFontScaling = false; 
+    Button.defaultProps.allowFontScaling = false;
+ 
     this.config = {
       en: {
         lang: `Hello, I am an ${this.OS} Expo app. This system's language setting is in English but we can override them here:`,
-        color: `I'm currently on `,
-        font: `This device's font is set to `
+        color: `The system is set to ${this.colorScheme} mode but you can override here:`,
+        font: `This device's font is set to ${this.fontScale} but only you can tell me what to do!`,
+        restore: 'Restore System Default',
+        buttons: ['Small','Medium','Large', 'Dark', 'Light']
       },
       fr: {
         lang: `Bonjour, je suis une application ${this.OS} Expo. Le paramètre de langue de ce système est en français, mais nous pouvons les remplacer ici:`,
-        color: `Je suis actuellement en mode `,
-        font: `La police de cet appareil est définie sur `
+        color: `Le système est réglé sur le mode ${this.colorScheme === 'dark' ? 'sombre' : 'lumière'} mais vous pouvez passer outre ici:`,
+        font: `La police de cet appareil est définie sur ${this.fontScale} mais vous seul pouvez me dire quoi faire !`,
+        restore: 'Restaurer les paramètres par défaut du système',
+        buttons: ['Petit','Moyen','Gros', 'Sombre', 'Lumière']
       },
       es: {
         lang: `Hola, soy una aplicación de ${this.OS}. La configuración de idioma de este sistema está en español, pero podemos anularla aquí: `,
-        color: `Actualmente estoy en modo `,
-        font: `La fuente de este dispositivo está configurada en `
+        color: `El sistema está configurado en modo ${this.colorScheme === 'dark' ? 'oscuro' : 'de luz'}, pero puede anularlo aquí:`,
+        font: `¡La fuente de este dispositivo está configurada en ${this.fontScale} pero solo tú puedes decirme qué hacer!`,
+        restore: 'Restaurar valores predeterminados del sistema',
+        buttons: ['Pequeño','Mediano','Grande', 'Oscuro', 'De Luz']
       }
     }
     
@@ -42,9 +49,8 @@ export default class App extends Component{
     this.styles = StyleSheet.create({
       container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
       },
       buttonBox: {
         flexDirection: 'row'
@@ -52,6 +58,12 @@ export default class App extends Component{
       button: {
         marginRight: 24,
         backgroundColor: 'black'
+      },
+      textColorLight: {
+        color: '#000'
+      },
+      textColorDark: {
+        color: '#fff'
       }
     });
     
@@ -59,12 +71,30 @@ export default class App extends Component{
       textValue: this.config[this.languageCode],
       colorScheme: this.colorScheme,
       fontScale: this.fontScale,
+      appState: AppState.currentState
     }
   }
-
+  componentDidMount() {
+    this.appStateSubscription = AppState.addEventListener(
+      'change',
+      nextAppState => {
+        if (
+          this.state.appState.match(/inactive|background/) &&
+          nextAppState === 'active'
+        ) {
+          console.log('App has come to the foreground!');
+          this.languageCode = getLocales()[0].languageCode;
+          this.colorScheme = Appearance.getColorScheme();
+          this.fontSize = 16;
+          this.fontScale = 'medium';
+          this.defaults = [getLocales()[0].languageCode, Appearance.getColorScheme(), 16];
+        }
+        this.setState({appState: nextAppState});
+      },
+    );
+  }
   getFontSize() {
     let fontScale = useWindowDimensions().fontScale;
-
     let textSize;
     switch (true) {
       case fontScale > 1.29:
@@ -98,19 +128,32 @@ export default class App extends Component{
     })
   }
   setFontSize(size) {
-    this.fontScale = size;
+    this.fontSize = size;
 
     this.setState({
-      fontScale: this.fontScale
+      fontSize: this.fontSize
+    })
+  }
+  restoreDefaults() {
+    this.languageCode = this.defaults[0];
+    this.colorScheme = this.defaults[1];
+    this.fontSize = this.defaults[2];
+    this.setState({
+      textValue: this.config[this.languageCode],
+      colorScheme: this.colorScheme,
+      fontSize: this.fontSize
     })
   }
 
   render() {
-    const { languageCode } = this.state;
     return (
-    <View style={this.styles.container}>
-      <Text>{this.state.textValue.lang}</Text>
-      {/* <Text>Hello, I am an {this.OS} Expo app. I speak {this.languageCode} but not very well.</Text> */}
+    <View 
+      style={this.styles.container}
+      backgroundColor={this.colorScheme === 'dark' ? '#555' : '#fff'}
+      >
+      <Text
+        style={[this.colorScheme === 'dark' ? this.styles.textColorDark : this.styles.textColorLight, {fontSize: this.fontSize}]}
+      >{this.state.textValue.lang}</Text>
       <View style={this.styles.buttonBox}>
         <Button
           style={this.styles.button}
@@ -119,51 +162,58 @@ export default class App extends Component{
         />
         <Button
           style={this.styles.button}
-          title="Spanish"
+          title="Español"
           onPress={() => this.setLanguageCode('es')}
         />
         <Button
           style={this.styles.button}
-          title="French"
+          title="Français"
           onPress={() => this.setLanguageCode('fr')}
         />
       </View>
-      {/* <Text>I'm currently on {this.colorScheme} mode. </Text> */}
-      <Text>{this.state.textValue.color}{this.colorScheme}</Text>
+      <Text
+        style={[this.colorScheme === 'dark' ? this.styles.textColorDark : this.styles.textColorLight, {fontSize: this.fontSize}]}
+      >{this.state.textValue.color}</Text>
 
       <View style={this.styles.buttonBox}>
           <Button
             style={this.styles.button}
-            title="Dark"
+            title={this.state.textValue.buttons[3]}
             onPress={() => this.setColorScheme('dark')}
           />
           <Button
             style={this.styles.button}
-            title="Light"
+            title={this.state.textValue.buttons[4]}
             onPress={() => this.setColorScheme('light')}
           />
       </View>
-      {/* <Text>This device's font is set to {this.fontScale}</Text> */}
-      <Text>{this.state.textValue.font}{this.fontScale}</Text>
+      <Text 
+        style={[this.colorScheme === 'dark' ? this.styles.textColorDark : this.styles.textColorLight, {fontSize: this.fontSize}]}
+      >{this.state.textValue.font}</Text>
 
       <View style={this.styles.buttonBox}>
           <Button
             style={this.styles.button}
-            title="Small"
-            onPress={() => this.setFontSize('small')}
+            title={this.state.textValue.buttons[0]}
+            onPress={() => this.setFontSize(16)}
           />
           <Button
             style={this.styles.button}
-            title="Medium"
-            onPress={() => this.setFontSize('medium')}
+            title={this.state.textValue.buttons[1]}
+            onPress={() => this.setFontSize(24)}
           />
           <Button
             style={this.styles.button}
-            title="Large"
-            onPress={() => this.setFontSize('large')}
+            title={this.state.textValue.buttons[2]}
+            onPress={() => this.setFontSize(32)}
           />
       </View>
       <StatusBar style="auto" />
+      <Button
+            style={this.styles.button}
+            title={this.state.textValue.restore}
+            onPress={() => this.restoreDefaults()}
+          />
     </View>
   );
 }};
